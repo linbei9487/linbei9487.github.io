504462f3739f338e28cca572a6ff50480807d5e7
bradly0cjw <bradly093@gmail.com>
2022-07-03 16:31:42 +0800

    errfix

--- src/js/test.js
+++ src/js/test.js
@@ -1,12 +1,3 @@
----
-title: testpage
-layout: testpage
-filename: testpage
---- 
-
-test<br>
-<p id="p0"></p>
-<script>
 var requestURL = "https://linbei9487.github.io/src/json/episode.json"
     var request = new XMLHttpRequest();
     request.open('GET', requestURL);
@@ -20,8 +11,7 @@ var requestURL = "https://linbei9487.github.io/src/json/episode.json"
     for (step=0; step <6 ; step++){
         document.getElementById("p"+ id).innerHTML =(data2.episode[id].pw)
         document.getElementById("p"+ id).innerHTML =(data2.episode[id].link)
-        console.log(ddata2.episode[id].pw)
+        console.log(data2.episode[id].pw)
         console.log(data2.episode[id].link)
         id++
         }};
-</script>
