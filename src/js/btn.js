7bdce11e687ce64759495681a7c1666464563f00
bradly0cjw <bradly093@gmail.com>
2022-07-03 15:32:06 +0800

    testpage

--- src/js/btn.js
+++ src/js/btn.js
@@ -1,6 +1,7 @@
       //Get the button
       // var mybutton = document.getElementById("buttontop");
-      
+      document.getElementById("buttonbottom").style.display = "block"
+      document.getElementById("buttontop").style.display = "none"
       // When the user scrolls down 20px from the top of the document, show the button
       window.onscroll = function() {scrollFunction()};
       
