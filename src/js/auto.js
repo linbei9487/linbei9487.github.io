d5c778f5899ce9e9ff1d0eb7d831d4318fa1088e
bradly0cjw <bradly093@gmail.com>
2022-07-04 20:08:47 +0800

    js fix

--- src/js/auto.js
+++ src/js/auto.js
@@ -10,44 +10,44 @@ var requestURL = "https://raw.githubusercontent.com/linbei9487/linbei9487.github
         var tit = document.createElement('h1');
         tit.innerHTML =("S3E"+ (data2.episode[step].index));
         tit.id = ("tit"+ step);
-        document.append(tit);
+        document.body.append(tit);
         var nam = document.createElement('h2');
         nam.innerHTML =((data2.episode[step].name));
         nam.id = ("nam"+ step);
-        document.append(nam);
+        document.body.append(nam);
         var img1 =document.createElement('img');
         img1.src =(data2.episode[step].img1);
         img1.id=("img1"+ step)
-        document.append(img1);
+        document.body.append(img1);
         var img2 =document.createElement('img');
         img2.src =(data2.episode[step].img2);
         img2.id=("img2"+ step)
-        document.append(img2);
+        document.body.append(img2);
         var img3 =document.createElement('img');
         img3.src =(data2.episode[step].img3);
         img3.id=("img3"+ step)
-        document.append(img3);
+        document.body.append(img3);
         var img4 =document.createElement('img');
         img4.src =(data2.episode[step].img4);
         img4.id=("img4"+ step)
-        document.append(img4);
+        document.body.append(img4);
         var eqe = document.createElement('h2');
         eqe.innerHTML =("進入小馬國");
         eqe.id = ("eqe"+ step);
-        document.append(eqe);
+        document.body.append(eqe);
         var lin = document.createElement('a');
         lin.innerHTML=("點擊此處")
         lin.href =((data2.episode[step].index));
         lin.id = ("lin"+ step);
-        document.append(lin);
+        document.body.append(lin);
         var par = document.createElement('p');
         par.innerHTML =("Password: "+ (data2.episode[step].pw));
         par.id = ("p"+ step);
-        document.append(par);
+        document.body.append(par);
         var vid = document.createElement('iframe');
         vid.src = (data2.episode[step].link);
         vid.id = ("m"+ step);
-        document.append(vid)
+        document.body.append(vid)
         // document.getElementById("p"+ step).innerHTML =("Password: "+ (data2.episode[step].pw));
         // document.getElementById("m"+ step).src =(data2.episode[step].link);
     
