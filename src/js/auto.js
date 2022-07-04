42707993b9648debb516d078b1177db02b5e193c
bradly0cjw <bradly093@gmail.com>
2022-07-04 16:44:28 +0800

    auto method test

---
+++ src/js/auto.js
@@ -0,0 +1,45 @@
+var requestURL = "https://raw.githubusercontent.com/linbei9487/linbei9487.github.io/main/src/json/test.json"
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    // request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+    var data2 = JSON.parse(request.responseText);
+    var step;
+    for (step=0; step < 3 ; step++){
+        var tit = document.createElement('h1');
+        tit.innerHTML =("S3E"+ (data2.episode[step].index));
+        tit.id = ("tit"+ step);
+        var nam = document.createElement('h2');
+        nam.innerHTML =((data2.episode[step].name));
+        nam.id = ("nam"+ step);
+        var img1 =document.createElement('img');
+        img1.src =(data2.episode[step].img1);
+        img1.id=("img1"+ step)
+        var img2 =document.createElement('img');
+        img2.src =(data2.episode[step].img2);
+        img2.id=("img2"+ step)
+        var img3 =document.createElement('img');
+        img3.src =(data2.episode[step].img3);
+        img3.id=("img3"+ step)
+        var img4 =document.createElement('img');
+        img4.src =(data2.episode[step].img4);
+        img4.id=("img4"+ step)
+        var eqe = document.createElement('h2');
+        eqe.innerHTML =("進入小馬國");
+        eqe.id = ("eqe"+ step);
+        var lin = document.createElement('a');
+        lin.innerHTML=("點擊此處")
+        lin.href =((data2.episode[step].index));
+        lin.id = ("lin"+ step);
+        var par = document.createElement('p');
+        par.innerHTML =("Password: "+ (data2.episode[step].pw));
+        par.id = ("p"+ step);
+        var vid = document.createElement('iframe');
+        vid.src(data2.episode[step].link);
+        vid.id = ("m"+ step);
+        document.append(tit,nam,img1,img2,img3,img4,eqe,lin,par,vid)
+        // document.getElementById("p"+ step).innerHTML =("Password: "+ (data2.episode[step].pw));
+        // document.getElementById("m"+ step).src =(data2.episode[step].link);
+    
+        }};
---
+++ testpage/loop.md
@@ -0,0 +1,7 @@
+---
+title: Twilight's Library Season 2
+layout: backpony
+filename: ponyG4S2
+---
+
+<script src=https://raw.githubusercontent.com/linbei9487/linbei9487.github.io/main/src/js/loop.js></script>
