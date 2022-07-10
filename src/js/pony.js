8bebb189987998be6d34ba6fe0c8f28250f5c641
bradly0cjw <bradly093@gmail.com>
2022-07-10 18:38:35 +0800

    add alert

--- src/js/pony.js
+++ src/js/pony.js
@@ -5,6 +5,7 @@ if (document.readyState == 'loading') {
 }
 function work() {
     if ((localStorage.getItem('ep') == null) || (localStorage.getItem('title') == null) || (localStorage.getItem('epanc') == null) || (localStorage.getItem('url') == null)) {
+        alert("歡迎來到本站，本頁將顯示日後的觀看進度")
     } else {
         document.getElementById("episode").innerHTML = (localStorage.getItem('ep'));
         document.getElementById("title").innerHTML = (localStorage.getItem('title'));
